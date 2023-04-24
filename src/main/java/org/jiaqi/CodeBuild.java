14938537062d3696150483820f93d33d533497cf
Ocean.O <zhangjiaqi9527@gmail.com>
2023-04-24 13:14:06 +0800

    Update CodeBuild.java

--- src/main/java/org/jiaqi/CodeBuild.java
+++ src/main/java/org/jiaqi/CodeBuild.java
@@ -19,47 +19,12 @@ import java.util.Properties;
  * @Author jiaqi.zhang
  * @Date 2023/2/20-17:50
  */
-public class CodeBuild {
-    public static Properties properties;
+public class CodeBuilder {
 
-    static {
-        properties = new CodeTempleConfiguration().getProperties();
-    }
+    private final Properties properties;
 
-    public static final String OPERATION = "operation";
-
-    String authorName = properties.getProperty("authorName");
-
-    String packageName = properties.getProperty("packageName");
-    String savePath = properties.getProperty("savePath");
-    String host = properties.getProperty("host");
-    String className = properties.getProperty("className");
-    String daoName = properties.getProperty("daoName");
-    String insert = properties.getProperty("insert");
-    String list = properties.getProperty("list");
-    String update = properties.getProperty("update");
-    String delete = properties.getProperty("delete");
-    String testPath = properties.getProperty("testPath");
-    String modelPath = properties.getProperty("modelPath");
-    String modulePath = properties.getProperty("modulePath");
-    String daoPath = properties.getProperty("daoPath");
-
-    String bossUser = properties.getProperty("bossUser");
-
-    /**
-     * VelocityEngine初始化
-     *
-     * @return
-     */
-    public VelocityEngine intiVelocityEngine() {
-        VelocityEngine velocityEngine = new VelocityEngine();
-        //设置模版和输出的代码文件的编码方式
-        Properties properties = new Properties();
-        properties.setProperty(RuntimeConstants.ENCODING_DEFAULT, CharEncoding.UTF_8);
-        properties.setProperty(RuntimeConstants.INPUT_ENCODING, CharEncoding.UTF_8);
-        properties.setProperty(RuntimeConstants.OUTPUT_ENCODING, CharEncoding.UTF_8);
-        velocityEngine.init(properties);
-        return velocityEngine;
+    public CodeBuilder(Properties properties) {
+        this.properties = properties;
     }
 
     /**
@@ -67,103 +32,73 @@ public class CodeBuild {
      *
      * @return
      */
-    public VelocityContext intiContext() {
-        VelocityContext context = new VelocityContext();
-        context.put("packageName", packageName);
-        context.put("className", className);
-        context.put("classNameLower", className.toLowerCase());
-        context.put("authorName", authorName);
-        context.put("daoName", daoName);
-        context.put("host", host);
-        context.put("bossUser", bossUser);
+    private Context initContext() {
+        Context context = new Context();
+        context.put("packageName", properties.getProperty("packageName"));
+        context.put("className", StringUtils.capitalize(properties.getProperty("className")));
+        context.put("classNameLower", properties.getProperty("className").toLowerCase());
+        context.put("authorName", properties.getProperty("authorName"));
+        context.put("daoName", StringUtils.capitalize(properties.getProperty("daoName")));
+        context.put("host", properties.getProperty("host"));
+        context.put("bossUser", properties.getProperty("bossUser"));
         return context;
     }
 
-    @SneakyThrows
-    public void build() {
-        codeBuild(intiVelocityEngine(), intiContext(),makeDir()+"/");
-    }
-
     /**
-     * 生成对于.vm的代码文件
-     *
-     * @param velocityEngine
-     * @param context
-     * @param
-     */
-    public void codeBuild(VelocityEngine velocityEngine, VelocityContext context, String dirName) {
-        System.out.println("dirName: "+dirName);
-        //引入模版，通过模版路径"test.vm" ,将占位符数据和模版合并，输出代码文件
-        Template testVm = velocityEngine.getTemplate(testPath);
-        merge(testVm, context, dirName + className + "ApiTest.java");
-
-        // 合并module文件
-        Template moduleVm = velocityEngine.getTemplate(modulePath);
-        merge(moduleVm, context, dirName + className + "Api.java");
-
-        ArrayList<String> operationList = new ArrayList<>();
-        operationList.add(insert);
-        operationList.add(list);
-        operationList.add(update);
-        operationList.add(delete);
-
-        for (String s : operationList) {
-            if (s.equals(insert)) {
-                context.put(OPERATION, StringUtils.capitalize(s));
-                Template modelVm = velocityEngine.getTemplate(modelPath);
-                System.out.println(dirName + s + className + "Model.java");
-                merge(modelVm, context, dirName + s + className + "Model.java");
-            }
-            if (s.equals(list)) {
-                context.put(OPERATION, StringUtils.capitalize(s));
-                Template modelVm = velocityEngine.getTemplate(modelPath);
-                merge(modelVm, context, dirName + s + className + "Model.java");
-            }
-            if (s.equals(update)) {
-                context.put(OPERATION, StringUtils.capitalize(s));
-                Template modelVm = velocityEngine.getTemplate(modelPath);
-                merge(modelVm, context, dirName + s + className + "Model.java");
-            }
-            if (s.equals(delete)) {
-                context.put(OPERATION, StringUtils.capitalize(s));
-                Template modelVm = velocityEngine.getTemplate(modelPath);
-                merge(modelVm, context, dirName + s + className + "Model.java");
-            }
-        }
-        // 合并dao文件
-        Template daoVm = velocityEngine.getTemplate(daoPath);
-        merge(daoVm, context, dirName + daoName + ".java");
-        System.out.println("成功");
-    }
-
-    /**
-     * 合并数据和模版，输出文件
+     * 合并数据和模板，输出文件
      *
      * @param template
      * @param ctx
      * @param path
      */
-    private void merge(Template template, VelocityContext ctx, String path) {
+    private void merge(Template template, Context ctx, String path) throws Exception {
         try (PrintWriter writer = new PrintWriter(path)) {
-            //合并数据和模版，输出文件
             template.merge(ctx, writer);
             writer.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
     /**
-     * 创建文件夹
+     * 构建测试用例代码
+     * @throws Exception 
      */
-    public String makeDir(){
-        File file = new File(savePath);
-        String dirName = savePath + properties.getProperty("className").toLowerCase();
-        if (file.length() == 0 || !new File(dirName).exists()) {
-            if (new File(dirName).mkdir()) {
-                return dirName;
-            }
+    public void build() throws Exception {
+    	
+    	// 初始化 Velocity 引擎
+    	VelocityEngine engine = new VelocityEngine();
+        engine.setProperty(RuntimeConstants.ENCODING_DEFAULT, CharEncoding.UTF_8);
+        engine.setProperty(RuntimeConstants.INPUT_ENCODING, CharEncoding.UTF_8);
+        engine.setProperty(RuntimeConstants.OUTPUT_ENCODING, CharEncoding.UTF_8);
+        
+        // 初始化 Velocity 上下文
+    	Context context = initContext();
+    	
+    	// 合并模板生成测试文件
+        String testFile = properties.getProperty("savePath") + "/" + context.get("className") + "ApiTest.java";
+        Template testVm = engine.getTemplate(properties.getProperty("testPath"));
+        merge(testVm, context, testFile);
+
+        // 合并模板生成模块文件
+        String moduleFile = properties.getProperty("savePath") + "/" + context.get("className") + "Api.java";
+        Template moduleVm = engine.getTemplate(properties.getProperty("modulePath"));
+        merge(moduleVm, context, moduleFile);
+
+        // 合并模板生成操作项对应的model文件
+        List<String> operationList = Arrays.asList("insert", "list", "update", "delete");
+        String modelPath = properties.getProperty("savePath") + "/" + StringUtils.capitalize(properties.getProperty("className"));
+        Template modelVm = engine.getTemplate(properties.getProperty("modelPath"));
+
+        for(String s : operationList) {
+            String modelFile = modelPath + StringUtils.capitalize(s) + "Model.java";
+            Context modelCtx = new Context(context);
+            modelCtx.put("operation", StringUtils.capitalize(s));
+            merge(modelVm, modelCtx, modelFile);
         }
-        return dirName;
+
+        // 合并模板生成dao文件
+        String daoFile = properties.getProperty("savePath") + "/" + StringUtils.capitalize(properties.getProperty("daoName")) + ".java";
+        Template daoVm = engine.getTemplate(properties.getProperty("daoPath"));
+        merge(daoVm, context, daoFile);
     }
 }
+
